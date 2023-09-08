917de9799e5b87e8ec24245d458eeba537eed5b1
umaru <15875339926@139.com>
2023-09-08 19:05:43 +0800

    firstinit

---
+++ ai_service/components/database.py
@@ -0,0 +1,10 @@
+from ai_service.components.base import BASE_DIR
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
---
+++ ai_service/settings.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+from split_settings.tools import include, optional
+
+include(
+    'components/base.py',
+    'components/database.py',
+    optional('local_settings.py')
+)
